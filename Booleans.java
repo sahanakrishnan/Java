30fb31a8b3922766433356b7a1ad61b01024d830
sahanakrishnan <sahanasr03@gmail.com>
2021-02-22 21:37:00 -0500

    Init
    Init

---
+++ Booleans.java
@@ -0,0 +1,8 @@
+
+public class Booleans {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 9;
+        System.out.println(x > y); // returns true, because 10 is higher than 9  
+      }
+}
---
+++ NarrowingCasting.java
@@ -0,0 +1,10 @@
+
+public class NarrowingCasting {
+    public static void main(String[] args) {
+        double myDouble = 9.78;
+        int myInt = (int) myDouble; // Explicit casting: double to int
+    
+        System.out.println(myDouble);
+        System.out.println(myInt);
+      }
+}
